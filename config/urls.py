b33f86385d5ad636070d3ca7ea85a207356c8ee6
gregory <gregorysak90@gmail.com>
2022-11-25 15:05:37 -0300

    feat: searchArea implemented

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from immovablehutting.views import home
+from immovablehutting.views import home, search
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', home),
+    path('search/', search),
 ]
--- immovablehutting/views.py
+++ immovablehutting/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
   return render(request, 'index.html')
+
+def search(request):
+  return render(request, 'searchArea.html')
