37fe5eeb16b97937bd8ba59f60fc43df3fff31a9
gregory <gregorysak90@gmail.com>
2022-11-26 11:17:37 -0300

    fix

--- immovablehutting/models.py
+++ immovablehutting/models.py
@@ -24,11 +24,8 @@ class Reserve(models.Model):
   annoucement_id = models.ForeignKey(Annoucement, on_delete=models.SET_NULL)
   total_price = models.FloatField(help_text="digite o valor da reserva total da reserva(ex: 952.38)")
   comment_field = models.CharField(max_length=255)
-  number_of_guests = models.ForeignKey(Immoble.guest_limit, on_delete=models.SET_NULL)
+  number_of_guests = models.IntegerField(max_length=6)
   check_in_date = models.DateField(auto_now_add=True)
   check_out_date = models.DateField(auto_now=True)
   date_time_create = models.DateTimeField(auto_now_add=True)
   date_time_update = models.DateTimeField(auto_now=True)
-  
-  def __str__(self):
-    return self.number_of_guests
