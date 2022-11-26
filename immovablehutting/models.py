791dab00b650ced6852fa41a2aa06b1b3489abdd
gregory <gregorysak90@gmail.com>
2022-11-26 10:31:23 -0300

    feat(backend): immoble model implemented

--- immovablehutting/models.py
+++ immovablehutting/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Immoble(models.Model):
+  title = models.CharField(max_length=255)
+  immoble_code = models.PositiveIntegerField(auto_created=True)
+  guest_limit = models.IntegerField(max_length=6)
+  number_of_bathorroms = models.IntegerField(max_length=3)
+  accepted_pet = models.BooleanField(default=False)
+  cleaning_price = models.IntegerField(max_length=3)
+  date_activation = models.DateTimeField(auto_now_add=True)
+  date_time_create = models.DateTimeField(auto_now_add=True)
+  date_time_update = models.DateTimeField(auto_now=True)
+
+class Annoucement(models.Model):
+  ...
+  
+class Reserve(models.Model):
+  ...
