a12a1a65d1c7d58c47ba7763a9a786240d0277a3
gregory <gregorysak90@gmail.com>
2022-11-27 17:14:10 -0300

    feat: models implementing

--- immovablehutting/models.py
+++ immovablehutting/models.py
@@ -10,31 +10,31 @@ class Immoble(models.Model):
     ('6','6'),
   ]
   
-  # title = models.CharField(max_length=255)
+  title = models.CharField(max_length=255)
   immoble_code = models.PositiveIntegerField(auto_created=True, unique=True)
   guest_limit = models.IntegerField(max_length=1, choices=guests)
-  # number_of_bathorroms = models.IntegerField(max_length=3)
-  # accepted_pet = models.BooleanField(default=False)
-  # cleaning_price = models.IntegerField(max_length=3)
-  # date_activation = models.DateTimeField(auto_now_add=True)
-  # date_time_create = models.DateTimeField(auto_now_add=True)
-  # date_time_update = models.DateTimeField(auto_now=True)
+  number_of_bathorroms = models.IntegerField(max_length=3)
+  accepted_pet = models.BooleanField(default=False)
+  cleaning_price = models.IntegerField(max_length=3)
+  date_activation = models.DateTimeField(auto_now_add=True)
+  date_time_create = models.DateTimeField(auto_now_add=True)
+  date_time_update = models.DateTimeField(auto_now=True)
 
 class Annoucement(models.Model):
   immoble_id = models.ForeignKey(Immoble, on_delete=models.CASCADE)
-  # publishing_plataform = models.CharField(max_length=50)
-  # plataform_rate = models.FloatField(help_text="digite a taxa da plataforma. ex: 13.00")
-  # date_time_create = models.DateTimeField(auto_now_add=True)
-  # date_time_update = models.DateTimeField(auto_now=True)
+  publishing_plataform = models.CharField(max_length=50)
+  plataform_rate = models.FloatField(help_text="digite a taxa da plataforma. ex: 13.00")
+  date_time_create = models.DateTimeField(auto_now_add=True)
+  date_time_update = models.DateTimeField(auto_now=True)
   
   
 class Reserve(models.Model):
-  # reserve_code = models.PositiveIntegerField(auto_created=True, unique=True)
+  reserve_code = models.PositiveIntegerField(auto_created=True, unique=True)
   annoucement_id = models.ForeignKey(Annoucement, on_delete=models.SET_NULL, related_name="id_annoucement", null=True)
-  # total_price = models.FloatField(help_text="digite o valor da reserva total da reserva(ex: 952.38)")
-  # comment_field = models.CharField(max_length=255)
+  total_price = models.FloatField(help_text="digite o valor da reserva total da reserva(ex: 952.38)")
+  comment_field = models.CharField(max_length=255)
   number_of_guests = models.ForeignKey(Immoble, on_delete=models.DO_NOTHING, related_name="limit_guests", null=True) # essa chave tem q ser estrangeira -> Immoble.guest_limit
-  # check_in_date = models.DateField(auto_now_add=True)
-  # check_out_date = models.DateField(auto_now=True)
-  # date_time_create = models.DateTimeField(auto_now_add=True)
-  # date_time_update = models.DateTimeField(auto_now=True)
+  check_in_date = models.DateField(auto_now_add=True)
+  check_out_date = models.DateField(auto_now=True)
+  date_time_create = models.DateTimeField(auto_now_add=True)
+  date_time_update = models.DateTimeField(auto_now=True)
