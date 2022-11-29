0ed7f6e96266071725b898fe5a3ee8b53d56d239
gregory <gregorysak90@gmail.com>
2022-11-29 01:17:05 -0300

    test: properly tested immoble model

--- tests/test_models.py
+++ tests/test_models.py
@@ -13,4 +13,4 @@ def test_immoble_model(immoble):
   immobles_from_db = Immoble.objects.all()
   
   assert len(immobles_from_db) == 1
-  assert immobles_from_db[0].title == 'Casa em Salinas'
+  assert immobles_from_db[0].title == "Casa em Salinas"
